f1b931efe5a17aae7094c4ba97c976910e2aa9a2
WilliamChung <William@Williams-Mac-mini.local>
2016-10-24 20:24:56 -0700

    added timestamp

--- Button Testing/ViewController.swift
+++ Button Testing/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var timer = Timer()
-    var counter = 0
+    //var timer = Timer()
+    //var counter = 0
+    
+    var currentDown: Double = 0
+    var oldDown: Double = 0
+    var currentUp: Double = 0
+    
+    var timeStamp: String {
+        return "\(NSDate().timeIntervalSince1970 * 1000)"
+    }
+    
 
     @IBOutlet weak var areaPress: UITextField!
     
@@ -26,24 +35,34 @@ class ViewController: UIViewController {
     
     @IBAction func push(_ sender: AnyObject) {
         
-        timer = Timer.scheduledTimer(timeInterval: 0.01, target:self, selector: #selector(ViewController.updateCounter), userInfo: nil, repeats: true)
+        //timer = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(ViewController.updateCounter), userInfo: nil, repeats: true)
+        
+        oldDown = currentDown
+        currentDown = 456//Double (timeStamp)!
+        
+        downDownTime.text = "1111"//String (currentDown - oldDown)
+        timerField.text = "2222"//String (timeStamp)
     }
 
     @IBAction func release(_ sender: AnyObject) {
-        timer.invalidate()
+        //timer.invalidate()
+        currentUp = 123//Double (timeStamp)!
+        upDownTime.text = "3333"//String (currentUp)
     }
     
-    func updateCounter() {
+    /*
+     func updateCounter() {
         
-        counter = counter + 0.01
-        timerField.text = String(counter)
+        //counter = counter + 1
+        //timerField.text = String(counter)
     }
+    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-            timerField.text = String(counter)
+            //timerField.text = String(counter)
         
     }
 
